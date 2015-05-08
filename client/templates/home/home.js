5ca2df5f2f3afba8be60277a7039ccd6338903d0
aacase <aacase@gmail.com>
2015-05-08 12:46:17 -0500

    Add googleMaps API and Jester King Homepage example

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -3,3 +3,29 @@ Template.home.rendered = function() {
      $('.parallax').parallax();
    });
 };
+
+
+Template.home.helpers({
+  jesterMapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(30.230659, -97.999296),
+        zoom: 17
+      };
+    }
+  }
+});
+
+
+Template.home.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('jesterKing', function(map) {
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    });
+  });
+});
